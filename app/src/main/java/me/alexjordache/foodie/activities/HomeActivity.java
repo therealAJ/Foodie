123e848a82b7de8c65e7a83231b5704f4b6670e6
Alex Jordache <alexonej@gmail.com>
2017-05-15 09:58:51 -0400

    refactor location

--- app/src/main/java/me/alexjordache/foodie/activities/HomeActivity.java
+++ app/src/main/java/me/alexjordache/foodie/activities/HomeActivity.java
@@ -1,12 +1,7 @@
 package me.alexjordache.foodie.activities;
 
-import android.Manifest;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.location.Location;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,29 +9,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
-import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
-import com.google.android.gms.location.LocationListener;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationServices;
-
 import me.alexjordache.foodie.R;
+import me.alexjordache.foodie.utils.LocationProvider;
 
-public class HomeActivity extends AppCompatActivity implements
-        ConnectionCallbacks,
-        OnConnectionFailedListener,
-        LocationListener {
+public class HomeActivity extends AppCompatActivity implements LocationProvider.LocationCallback {
 
-    protected static final String TAG = "HomeActivity";
-    protected GoogleApiClient mGoogleApiClient;
-    protected LocationRequest mLocationRequest;
-    protected Location mLastLocation;
-    protected String mLatitudeText;
-    protected String mLongitudeText;
-    protected TextView mLatitudeTextView;
-    protected TextView mLongitudeTextView;
+    private static final String TAG = "HomeActivity";
+    private LocationProvider mLocationProvider;
+    private TextView mLatitudeText;
+    private TextView mLongitudeText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,69 +26,33 @@ public class HomeActivity extends AppCompatActivity implements
         setContentView(R.layout.activity_home);
 
         //UI Elements
-//        mLatitudeTextView = (TextView) findViewById(R.id.latitude_text);
-//        mLongitudeTextView = (TextView) findViewById(R.id.longitude_text);
+        mLatitudeText = (TextView) findViewById(R.id.latitude_text);
+        mLongitudeText = (TextView) findViewById(R.id.longitude_text);
 
         Button findRestaurantsButton = (Button) findViewById(R.id.find_restaurants_btn);
 
+        mLocationProvider = new LocationProvider(this, this);
 
         //Initialize Listeners
         findRestaurantsButton.setOnClickListener(HandleFindRestaurantsClick);
 
-        buildGoogleApiClient();
-        createLocationRequest();
     }
 
-    private synchronized void buildGoogleApiClient() {
-        Log.i(TAG, "Building GoogleApiClient");
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .addApi(LocationServices.API)
-                .build();
-    }
-
-    private void createLocationRequest() {
-        mLocationRequest = new LocationRequest();
-    }
-
-    protected void onStart() {
-        mGoogleApiClient.connect();
-        super.onStart();
-    }
-
-    protected void onStop() {
-        mGoogleApiClient.disconnect();
-        super.onStop();
-    }
 
 
     @Override
-    public void onConnected(@Nullable Bundle bundle) {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            return;
-        }
-        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-        if (mLastLocation != null) {
-            mLatitudeText = (String.valueOf(mLastLocation.getLatitude()));
-            mLongitudeText = (String.valueOf(mLastLocation.getLongitude()));
-        }
+    protected void onResume() {
+        super.onResume();
+        mLocationProvider.connect();
     }
 
     @Override
-    public void onConnectionSuspended(int i) {
-
+    protected void onPause() {
+        super.onPause();
+        mLocationProvider.disconnect();
     }
 
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
-    }
-
-    @Override
-    public void onLocationChanged(Location location) {
-
-    }
 
     // Click Listeners
     View.OnClickListener HandleFindRestaurantsClick = new Button.OnClickListener() {
@@ -118,4 +64,11 @@ public class HomeActivity extends AppCompatActivity implements
         }
     };
 
+    @Override
+    public void handleNewLocation(Location location) {
+        Log.d(TAG, location.toString());
+
+        double currentLatitude = location.getLatitude();
+        double currentLongitude = location.getLongitude();
+    }
 }
